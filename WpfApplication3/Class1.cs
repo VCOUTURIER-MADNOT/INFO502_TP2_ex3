e65faace47f990e4cde9c0baebc696eafb1b66b0
coutcout <valcou@aliceadsl.fr>
2013-11-27 01:05:12 +0100

    Commencement ex3

---
+++ WpfApplication3/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3
+{
+    enum Dimension{ dim2 = "2D", dim3 = "3D" };
+
+    class CImage
+    {
+        private String name { get; set; }
+        private Int32 imageType { get; set; }
+        private Dimension imageDimension { get; set; }
+        private int height { get; set; }
+        private int width { get; set; }
+        private int depth { get; set; }
+        private int size { get; set; }
+        private Pixel[] pixels { get; set; }
+        private int[] histogramme { get; set; }
+
+        public CImage()
+        {
+            this.name = "";
+            this.imageType = 0;
+            this.imageDimension = Dimension.dim2;
+            this.height = 0;
+            this.width = 0;
+            this.depth = 0;
+            this.size = 0;
+            this.histogramme = new int[256];
+            
+        }
+        
+        public CImage(String name, Int32 imagetype, Dimension imageDimension)
+        {
+            this.name = name;
+            this.imageType = imageType;
+            this.imageDimension = imageDimension;
+        }
+
+
+
+    }
+}
---
+++ WpfApplication3/Pixel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3
+{
+    class Pixel
+    {
+        private int red { get; set; }
+        private int green { get; set; }
+        private int blue { get; set; }
+    }
+}
